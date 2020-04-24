a170c4e3eaa0aa82d45e7f76f8df5e175f786302
psilvao <psilvao@gmail.com>
2020-04-24 12:23:02 -0400

    agregando geolocalizacion

---
+++ js/geo.js
@@ -0,0 +1,17 @@
+//function that gets the location and returns it
+function getLocation() {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geo Location not supported by browser");
+  }
+}
+//function that retrieves the position
+function showPosition(position) {
+  var location = {
+    longitude: position.coords.longitude,
+    latitude: position.coords.latitude
+  }
+  console.log(location)
+  document.getElementById("ubicacion").innerHTML=location;
+}
